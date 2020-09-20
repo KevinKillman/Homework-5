fb49dfaedfb3ab909cabcf2cb671488ed34064d1
Kevin Killman <kevinkillman@gmail.com>
2020-09-20 12:05:50 -0400

    I think I am done. FINAL FINAL

--- Instructions/Pymaceuticals/.ipynb_checkpoints/outlier_function-checkpoint.py
+++ Instructions/Pymaceuticals/.ipynb_checkpoints/outlier_function-checkpoint.py
@@ -1,6 +1,7 @@
 
 def outlier(series):
     import pandas as pd
+    index_counter = 0
     describe_series = series.describe()
     q1 = describe_series['25%']
     q3 = describe_series['75%']
@@ -8,11 +9,14 @@ def outlier(series):
     lower_bound = q1 - (1.5 * iqr)
     upper_bound = q3 + (1.5 * iqr)
     outliers = []
-    for (value) in series:
+    for (value) in series:        
         value = int(value)
         if value>upper_bound or value<lower_bound:
-            outliers.append(value)
-    return {'Lower Bound': lower_bound,
+            outliers.append(index_counter)
+        index_counter+=1
+    return {'Mean': series.mean(),
+            'Mode':series.mode(),
+            'Lower Bound': lower_bound,
             'Upper Bound': upper_bound,
             'Median': describe_series['50%'],
             'IQR': iqr,
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Homework-5
  
+In this assignment, I was given two datasets examining drug tests on mice and how it affects tumors that are present in the mice. I was given different measurements to obtain and display and also told to chart data in different ways. I observed trends regarding mouse weight versus average tumor volume and drug effectiveness.
